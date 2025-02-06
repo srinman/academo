9965c9d2ad39aa078f8487546c9c3fd9c777ac9d
srinman <smanivel@microsoft.com>
2025-02-06 09:29:14 -0500

    updates

--- dynamicsessions.py
+++ dynamicsessions.py
@@ -1,19 +1,20 @@
 import requests
 import os
-   
+from azure.identity import DefaultAzureCredential
+
+credential = DefaultAzureCredential()
+token = credential.get_token("https://dynamicsessions.io/.default")
+access_token = token.token
+# print(access_token)
 # use the following command to get authtoken
 # az account get-access-token --resource https://dynamicsessions.io
 # Configuration
 endpoint = "https://westus2.dynamicsessions.io/subscriptions/3eef5dad-ad68-4246-8e02-e13d661de047/resourceGroups/academorg/sessionPools/my-session-pool/code/execute?api-version=2024-02-02-preview&identifier=my-session-pool"
-authorization_token = os.getenv('AUTHORIZATION_TOKEN')
-
-if not authorization_token:
-    raise ValueError("Authorization token not found in environment variables")
 
 # Headers
 headers = {
     "Content-Type": "application/json",
-    "Authorization": f"Bearer {authorization_token}"
+    "Authorization": f"Bearer {access_token}"
 }
 
 # Payload
